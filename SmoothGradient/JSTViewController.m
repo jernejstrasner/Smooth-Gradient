75bd746948ee322154e6bb938aa10d7177534064
Jernej Strasner <jernej.strasner@gmail.com>
2015-06-14 23:17:58 +0200

    Updated app for all devices

--- SmoothGradient/JSTViewController.m
+++ SmoothGradient/JSTViewController.m
@@ -9,23 +9,7 @@
 #import "JSTViewController.h"
 #import "JSTGradientView.h"
 
-@interface JSTViewController ()
-
-@end
-
 @implementation JSTViewController
-
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 - (IBAction)switchMode:(UISegmentedControl *)sender
 {
